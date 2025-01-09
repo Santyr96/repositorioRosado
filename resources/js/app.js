7753c081905d7b4508408b35456ec97198aa135c
Santiago Rosado Ruiz <santiago.rosado@outlook.es>
2025-01-09 22:37:57 +0100

    Last version

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
 import './bootstrap';
+import './components/dashboards/admin-panel-extend'
+import './components/dashboards/load-view'
+import'./components/dashboards/notification-extend'
+import './auth/auth'
+import './auth/validate-hairdresser'
+import './auth/validate-profile.js'
+import './auth/validate-reset-password.js'
+import './components/dashboards/calendar/show-calendar.js'
+import './components/dashboards/calendar/modal-appointment-edit-and-delete.js'
+import './components/dashboards/calendar/modal-appointment-create.js'
+import './components/dashboards/hairdresser/insert-hairdresser.js'
+import './components/dashboards/hairdresser/delete-hairdresser.js'
+import './components/dashboards/hairdresser/reload-select-hairdresser-view.js'
+import './components/dashboards/profile/update-avatar.js'
+import './components/dashboards/profile/update-profile.js'
+import './components/dashboards/services-hair/create-service.js'
+import './components/dashboards/services-hair/delete-service.js'
+import './components/dashboards/services-hair/update-service.js'
+import './components/dashboards/services-hair/services-manage.js'
+import './components/dashboards/signup/signup-hairdresser.js'
+import './components/layouts/flyout_menu.js'
+import './components/modals/close-modal.js'
+import './components/passwords/show-password.js'
+
+
+
