a2c1f8f88bb9417e979ebb22b6a871db5f910b62
Santiago Rosado Ruiz <santiago.rosado@outlook.es>
2025-01-10 19:46:26 +0100

    cambios

--- resources/views/components/inputs/input.blade.php
+++ resources/views/components/inputs/input.blade.php
@@ -1,5 +1,5 @@
 <input class="w-full border-b-4 h-12 border-black text-sm pl-1 mr-1 focus:outline-none md:text-lg {{ $class ?? '' }}"
     type="{{ $type ?? 'text' }}" 
     name="{{ $name }}" 
-    placeholder="{{ $placeholder }}" 
+    placeholder="{{ $placeholder ?? '' }}" 
     value="{{ $value ?? '' }}" />
