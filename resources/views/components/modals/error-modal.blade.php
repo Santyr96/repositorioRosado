c871ea7cf77e2b643029761f762105e7f3f5a0f6
Santiago Rosado Ruiz <santiago.rosado@outlook.es>
2025-01-10 20:03:26 +0100

    cambios

--- resources/views/components/modals/error-modal.blade.php
+++ resources/views/components/modals/error-modal.blade.php
@@ -1,12 +1,12 @@
 @vite('resources/js/components/modals/close-modal.js')
 <div id='errorModal' tabindex="-1"
-    class=" flex justify-center items-center fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full {{$class}}">
+    class=" flex justify-center items-center fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full {{$class ?? ''}}">
     <div class="relative w-full max-w-md max-h-full">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
 
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                 <h3 id="errorModalTitle" class="text-xl font-medium text-gray-900 dark:text-white">
-                {{$modalTitle}}
+                {{$modalTitle ?? ''}}
                 </h3>
                 <button type="button"
                     class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -22,7 +22,7 @@
 
             <div class="p-4 md:p-5 space-y-4">
                 <p id="modalMessage" class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    {{$modalMessage}}
+                    {{$modalMessage ?? ''}}
                 </p>
               
             </div>
