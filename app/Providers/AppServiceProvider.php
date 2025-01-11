a7be7999c9b50c085df94df9942fc233042f574a
Santiago Rosado Ruiz <santiago.rosado@outlook.es>
2025-01-11 10:37:43 +0100

    cambios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\Vite;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Vite as FacadesVite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment('production')) {
+            FacadesVite::useBuildDirectory('build');
+        }
     }
 }
