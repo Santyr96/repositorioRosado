6e47f1c8e447fe46388f93e67cea6974f4323b4f
Santiago Rosado Ruiz <santiago.rosado@outlook.es>
2025-01-18 10:07:54 +0100

    cambios

---
+++ public/build/assets/show-calendar-BHlVdtgo.js
@@ -0,0 +1,1 @@
+import"./close-modal-McXQp2uh.js";import{r as e,s as o,a as d}from"./modal-appointment-edit-and-delete-wXjhEATs.js";export{e as renderCalendar,o as showCalendar,d as showErrorMessage};
--- public/build/assets/show-calendar-DEjsAZeQ.js
+++
@@ -1,1 +0,0 @@
-import"./close-modal-McXQp2uh.js";import{r as e,s as o,a as d}from"./modal-appointment-edit-and-delete-CSPJEa_t.js";export{e as renderCalendar,o as showCalendar,d as showErrorMessage};
